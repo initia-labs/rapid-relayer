3e83c80938762b2a3ea1c6ee1bfcd2c8350cbafe
ALPAC-4 <contact.alpac4@gmail.com>
2024-11-01 11:56:16 +0900

    add metric app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-// import { Chain } from './chain'
 import express from 'express'
-// import { runPair } from './lib/chainPair'
-// import { config } from './lib/config'
-// import { ChainStatus } from './chain/types'
-// import { registery } from './lib/metric'
+import { registery } from './lib/metric'
 import { info } from './lib/logger'
 
 import { config } from './lib/config'
@@ -17,23 +13,23 @@ async function main() {
   app.get('/status', (req, res) => {
     res.json(workerController.getStatus())
   })
-  // const metricApp = express()
+  const metricApp = express()
 
-  // metricApp.get('/metrics', (req, res) => {
-  //   res.setHeader('content-type', registery.contentType)
-  //   registery
-  //     .metrics()
-  //     .then((response) => res.send(response))
-  //     .catch(() => res.status(500).send('Fail to get metrics'))
-  // })
+  metricApp.get('/metrics', (req, res) => {
+    res.setHeader('content-type', registery.contentType)
+    registery
+      .metrics()
+      .then((response) => res.send(response))
+      .catch(() => res.status(500).send('Fail to get metrics'))
+  })
 
   app.listen(config.port)
   info(`status app listen to port ${config.port}`)
   info(`rapid relayer has been started`)
   info(JSON.stringify(workerController.getStatus(), undefined, 2))
 
-  // metricApp.listen(config.metricPort)
-  // info(`metric app listen to port ${config.metricPort}`)
+  metricApp.listen(config.metricPort)
+  info(`metric app listen to port ${config.metricPort}`)
 }
 
 void main()
